f51089e01b0e12c09c7f572d2fa499d6b2dc473e
HypermorphicGaming <xenomorphic120@gmail.com>
2025-09-25 14:49:49 -0400

    Started working on the loader for the data
    The idea is a 2D ArrayList, since we need to be able to add/remove books.

--- Project1/src/projectManagement.java
+++ Project1/src/projectManagement.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+
 public class projectManagement {
+    public static ArrayList<ArrayList<String>> loader(){
+        ArrayList<String> InitialData = new ArrayList<>();
+        ArrayList<ArrayList<String>> loadedData= new ArrayList<>();
+        
+        return loadedData;
+    }
+
     public static void main(String[] args) {
         
     }
